62d8a7042d44b2586d5219f32914ba706012923a
YounghwanYoon <ray.yoon87@gmail.com>
2019-03-26 19:51:50 -0400

    Working on pop up window

--- app/src/main/java/com/ray/sqlitetemplate/MyArrayAdapter.kt
+++ app/src/main/java/com/ray/sqlitetemplate/MyArrayAdapter.kt
@@ -2,7 +2,11 @@ package com.ray.sqlitetemplate
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Bundle
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v7.app.AppCompatActivity
+import android.text.Layout
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,17 +39,42 @@ class MyArrayAdapter(context: Context , val resource:Int, users:ArrayList<Pair<L
         userPW!!.text = user.second.mLoginPW
 
         //setOnItemClickListener
-        eachView.setOnLongClickListener(View.OnLongClickListener{
+        eachView.setOnLongClickListener(View.OnLongClickListener{ view ->
+            when(view.id){
+                R.id.login_id_text -> {
+                    //Initialize a layout inflater instance
+                    val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                    //inflate a custom view  using layout inflater
+                    val view = inflater.inflate(R.layout.popup_window, null)
+                    val popUpView:PopupWindow = PopupWindow(view,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                    popUpView.elevation = 10.0F
+
+                    //Set pop up window buttons
+                    val removeButton:Button = view.findViewById(R.id.remove_button)
+                    val updateButton:Button = view.findViewById(R.id.update_button)
+
+                    //Set up button a onClick Listern
+                    removeButton.setOnClickListener{object:View.OnClickListener{
+                            override fun onClick(p0: View?) {
+                                Toast.makeText(context, "Works!", Toast.LENGTH_SHORT ).show()
+                            }
+                        }}
+                    updateButton.setOnClickListener{object:View.OnClickListener{
+                        override fun onClick(p0: View?) {
+                            Toast.makeText(context, "Works!", Toast.LENGTH_SHORT ).show()
+                        }
+                    }}
+//https://android--code.blogspot.com/2018/02/android-kotlin-popup-window-example.html
+                    popUpView.showAtLocation()
+
+                }
+            }
             Toast.makeText(context,"I am clicked for a long time", Toast.LENGTH_SHORT).show()
             true
         })
-
         // Return the completed view to render on screen
         return eachView
     }
-
-    class popUpActivity():AppCompatActivity(){
-
-    }
-
 }
+
