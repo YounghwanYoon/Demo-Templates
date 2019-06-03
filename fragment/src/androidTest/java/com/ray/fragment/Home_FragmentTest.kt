b4253e4b597384d2a5f58be861d811b2631ff14f
YounghwanYoon <ray.yoon87@gmail.com>
2019-06-03 13:12:14 -0400

    https://www.youtube.com/watch?v=JoFN10FDm8U
    Learning Testing Drive Development (Fragment)

---
+++ fragment/src/androidTest/java/com/ray/fragment/Home_FragmentTest.kt
@@ -0,0 +1,19 @@
+package com.ray.fragment
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class Home_FragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    
+
+    @After
+    fun tearDown() {
+    }
+}
