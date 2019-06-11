3b4fee9ff45e7da01d0dda431e03999164ba12bb
YounghwanYoon <ray.yoon87@gmail.com>
2019-06-11 14:05:36 -0400

    Organize files

---
+++ srt_converter/src/main/java/com/ray/srt_converter/view/srt_activity.kt
@@ -0,0 +1,15 @@
+package com.ray.srt_converter.view
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ray.srt_converter.R
+
+class srt_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_srt_activity)
+
+    }
+}
