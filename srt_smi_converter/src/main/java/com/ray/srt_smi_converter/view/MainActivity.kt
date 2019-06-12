00a52e86ca7c932425f52666410093b6dacd5395
YounghwanYoon <ray.yoon87@gmail.com>
2019-06-12 18:07:17 -0400

    Organize files

--- srt_smi_converter/src/main/java/com/ray/srt_smi_converter/view/MainActivity.kt
+++ srt_smi_converter/src/main/java/com/ray/srt_smi_converter/view/MainActivity.kt
@@ -10,6 +10,7 @@ import com.ray.srt_smi_converter.view.adapter.StatePagerAdapter
 class MainActivity : FragmentActivity() {
     private var mViewAdapter= StatePagerAdapter(supportFragmentManager)
     lateinit var mViewPager:ViewPager
+    val selectBtn = findViewById<Button>(R.id.select_file_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +18,19 @@ class MainActivity : FragmentActivity() {
 
         mViewPager = findViewById(R.id.container_viewpager)
         setupViewPager(mViewPager, mViewAdapter)
-
-
-        val selectBtn = findViewById<Button>(R.id.select_file_button)
-        selectBtn.setOnClickListener {
-            mViewPager.currentItem = 1
-        }
+        button_setup()
     }
 
     fun setupViewPager(viewPager: ViewPager, adapter: StatePagerAdapter){
         adapter.addFragment(Select_Type_Fragment())
         adapter.addFragment(List_Of_Files())
         viewPager.adapter = adapter
+    }
+
+    fun button_setup(){
+        selectBtn.setOnClickListener {
+            mViewPager.currentItem = 1
+        }
+
     }
 }
